7324314e182c7dc4c3c73f747364457086ce57bd
IbrahimMahmoud-0 <0ibrahimmahmoud@gmail.com>
2024-05-14 12:39:45 +0300

    filter Bookings

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -37,11 +37,13 @@ const FilterButton = styled.button`
 
 function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const currentFilter = searchParams.get("discount") || options.at(0).value;
+  const currentFilter =
+    searchParams.get(`${filterField}`) || options.at(0).value;
   function handleClick(value) {
     searchParams.set(filterField, value);
     setSearchParams(searchParams);
   }
+
   return (
     <StyledFilter>
       {options.map(function (option) {
