0c907ae604a09226050e9bf15bec43c0b97ddd1b
= <=>
2024-05-25 15:02:04 +0300

    SignUpForm

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,13 @@
 import Heading from "../ui/Heading";
+import SignupForm from "../features/authentication/SignupForm";
 
 function NewUsers() {
-  return <Heading as="h1">Create a new user</Heading>;
+  return (
+    <div>
+      <Heading as="h1">Create a new user</Heading>
+      <SignupForm />
+    </div>
+  );
 }
 
 export default NewUsers;
